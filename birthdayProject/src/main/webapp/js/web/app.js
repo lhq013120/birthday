6726e34eee860873190779bd4fdac11b246374fd
skylightbbj <skylightbbj99@163.com>
2016-12-05 15:38:55 +0800

    .

--- birthdayProject/src/main/webapp/js/web/app.js
+++ birthdayProject/src/main/webapp/js/web/app.js
@@ -1,8 +1,13 @@
-var app=angular.module("app",[]);
-app.config(function($routeProvider)
+var app=angular.module("app",['ngRoute']);
+app.config(['$routeProvider',function($routeProvider)
 {
 	$routeProvider
 	.when("/login",{
 		templateUrl:"login.html"
-	}).otherCapture("/login");
-})
+	})//.otherCapture("/login");
+}])
+//
+//app.controller("app",function($scope)
+//{
+//	
+//})
