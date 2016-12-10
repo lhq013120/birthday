28a501518d618c4471b1f2d23ebe67d500b6763f
skylightbbj <skylightbbj99@163.com>
2016-12-10 10:52:04 +0800

    .

--- birthdayProject/src/main/webapp/js/web/app.js
+++ birthdayProject/src/main/webapp/js/web/app.js
@@ -1,13 +1,27 @@
 var app=angular.module("app",['ngRoute']);
-app.config(['$routeProvider',function($routeProvider)
+app.config(['$routeProvider','$httpProvider',function($routeProvider,$httpProvider)
 {
+ 	if (!$httpProvider.defaults.headers.get) {
+      $httpProvider.defaults.headers.get = {};
+    }
+    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+    
 	$routeProvider
 	.when("/",{
 		templateUrl:"login.html"
 	})
 	.when("/login",{
 		templateUrl:"login.html"
-	}).otherwise({redirectTo:"/"});
+	})
+	.when("/longevity",{
+		templateUrl:"longevity.html"
+	})
+	.when("/companyBenediction",{
+		templateUrl:"companyBenediction.html"
+	})
+	.otherwise({redirectTo:"/"});
 }])
 
 // angular.module("app", [
